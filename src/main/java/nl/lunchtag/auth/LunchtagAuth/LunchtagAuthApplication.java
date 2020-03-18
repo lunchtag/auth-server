3efb6cbc5282860ee151912c9399a4738b42577f
Manders,Rens R.A <r.manders@student.fontys.nl>
2020-03-18 11:03:58 +0100

    Initial commit

---
+++ src/main/java/nl/lunchtag/auth/LunchtagAuth/LunchtagAuthApplication.java
@@ -0,0 +1,13 @@
+package nl.lunchtag.auth.LunchtagAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchtagAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchtagAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/lunchtag/auth/LunchtagAuth/LunchtagAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.lunchtag.auth.LunchtagAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchtagAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
